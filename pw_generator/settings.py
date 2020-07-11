6a1427b5398f89b57304a88522129bcec0a67957
Ryan Adam <ryancadam88@gmail.com>
2020-07-11 13:11:55 -0500

    latest

--- pw_generator/settings.py
+++ pw_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%3hsg$avp(bf)q(!xhn1m@328)*nd+89le%vb@6tg8uy00^r@!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
